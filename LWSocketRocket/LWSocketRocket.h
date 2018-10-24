8af26cb27199d5e1affa28846e3f1eafe8f91cc7
shadon-wly <wly1250623585gmail.com>
2018-10-24 16:07:55 +0800

    first commit

---
+++ LWSocketRocket/LWSocketRocket.h
@@ -0,0 +1,20 @@
+//
+//  LWSocketRocket.h
+//  LWSocketRocket
+//
+//  Created by CPX on 2018/10/24.
+//  Copyright © 2018 CPX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LWSocketRocket.
+FOUNDATION_EXPORT double LWSocketRocketVersionNumber;
+
+//! Project version string for LWSocketRocket.
+FOUNDATION_EXPORT const unsigned char LWSocketRocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LWSocketRocket/PublicHeader.h>
+
+
+#import "SocketRocket.h"
